93df5a2564354203d47fcec7a7fd54a203604407
slavo3dev <slavo@slavo.io>
2022-12-31 21:48:21 -0500

    fixing

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,7 @@ export default function Home() {
     <>
       <Head>
         <title>Lion Nomad</title>
-        <meta name="description" content="Share Your Work / Progress and Learning - Gorwth" />
+        <meta name="description" content="Share Your Work & progress in learning or developing!!" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
